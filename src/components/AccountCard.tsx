79e6d8762220e08a331d6abad73afd65028256d0
Igor Andrade <profissional@igorandrade.com>
2026-01-16 20:29:18 -0300

    chore: change js to ts

--- src/components/AccountCard.tsx
+++ src/components/AccountCard.tsx
@@ -1,4 +1,9 @@
-export default function AccountCard({ account }) {
+interface Account {
+  name: string;
+  balance: number;
+}
+
+export default function AccountCard({ account }: { account: Account }) {
   const formattedBalance = account.balance.toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
